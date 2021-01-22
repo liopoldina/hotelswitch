98ed8e2cd47a04e537b358f7cd4ea5199f0491b9
liopoldina <61992095+liopoldina@users.noreply.github.com>
2021-01-22 11:56:13 +0000

    Change folder structure

--- app/Repositories/SearchRepository.php
+++ app/Repositories/SearchRepository.php
@@ -101,7 +101,7 @@ class SearchRepository{
         }
         
         if(count($hotels) > 1){
-        $process = new Process(['python', 'C:\wamp64\www\hotelhopping.com\science\client.py',$m->collection_name]);
+        $process = new Process(['python', 'C:\wamp64\www\hotelswitch\science\client.py',$m->collection_name]);
         $process->run();
 
         // executes after the command finishes
